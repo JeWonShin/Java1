54cf6894eb28a8a6b55d81cc0c4d00f25f2feee4
JeWonShin <46281735+JeWonShin@users.noreply.github.com>
2020-12-28 00:26:18 +0900

    java1_datatype_Number_String

---
+++ Data_and_operation/Datatype.java
@@ -0,0 +1,24 @@
+public class Datatype{
+	public static void main(String[] args) {
+		System.out.println(6);  // Number
+		System.out.println("six");  // String
+		
+		System.out.println("6"); // String 6  큰 따옴표로 묶어주면 보기에는 숫자라도 문자열이다.
+		
+		System.out.println(6+6); // 12
+		System.out.println("6"+"6"); // 66 , 이 떄의 +는 문자끼리 합쳐주는 결합연산자로 기능
+		
+		System.out.println(6*6); //36
+		// System.out.println("6"*"6");   문자열은 곱하기 안된다.
+		
+		System.out.println("1111".length()); // 4 , 문자열의 길이를 알려줌
+		// System.out.println(1111.length());  숫자는 길이를 알려주는 방법이 없다.
+		
+		//프로그래밍에서는 데이터 타입이 있다. 데이터타입을 구분하는 이유는 데이터타입마다 어울리는 연산방법이 있기 때문이다.
+		//컴퓨터를 다루는 방법의 스킬을 쌓는 것은 두가지의 중요한 흐름을 가진다.
+		//1. 내가 지금 하고 있는 시스템은 어떠한 데이터 타입들이 존재하는가.
+		//2. 각각 데이터 타입별로 어떤 연산방법이 존재하는가.  
+		//-> 이것들을 통해서 컴퓨터로 할 수 있는 일의 가능성이 증가
+		// 데이터 타입을 구분하는 이유? 숫자와 문자열을 표현하는 방법?
+	}
+}
